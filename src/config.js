ca8fec68d9131541872241c319efb62ecfc35119
John Wellbrock <wellbrock23john@gmail.com>
2020-12-04 16:07:58 -0700

    update

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 export default {
-	REACT_APP_API_URL: process.env.REACT_APP_API_URL || "http://localhost:8000",
+	REACT_APP_API_URL:
+		process.env.REACT_APP_API_URL ||
+		"https://mighty-woodland-15089.herokuapp.com",
 };
